61a3911fce353c45b4a7d8bfd4bc8c21b5b7e361
nunojesus22 <nunojesus446@gmail.com>
2024-01-12 14:53:54 +0000

    Corrigido problema do botao
    Está me a faltar os inputs

--- WatchersWorld/WatchersWorld.Server/obj/Debug/net8.0/WatchersWorld.Server.AssemblyInfo.cs
+++ WatchersWorld/WatchersWorld.Server/obj/Debug/net8.0/WatchersWorld.Server.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WatchersWorld.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bd15fc3367e51db770da554f062b56a392fb9ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("WatchersWorld.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WatchersWorld.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- WatchersWorld/watchersworld.client/src/app/app.module.ts
+++ WatchersWorld/watchersworld.client/src/app/app.module.ts
@@ -5,23 +5,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthenticationButtonComponent } from './authentication/components/authentication-button/authentication-button.component';
-import { AuthenticationInputComponent } from './authentication/components/authentication-input/authentication-input.component';
 import { LoginComponent } from './authentication/login/login.component';
 import { HomeComponent } from './home/home.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faGoogle } from '@fortawesome/free-brands-svg-icons';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthenticationButtonComponent,
-    AuthenticationInputComponent,
     LoginComponent,
     HomeComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule, FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(library: FaIconLibrary) {
+    library.addIcons(faGoogle);
+  }
+}
