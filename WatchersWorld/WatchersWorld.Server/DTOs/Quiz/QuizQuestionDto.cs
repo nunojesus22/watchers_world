62efccc83b9a0a7f04e47dca5c24fe9b8dd62d7e
201600286-Paulo <201600286@estudantes.ips.pt>
2024-03-12 02:39:10 +0000

    QuizQuestionDto - forgot

--- WatchersWorld/WatchersWorld.Server/DTOs/Quiz/QuizQuestionDto.cs
+++ WatchersWorld/WatchersWorld.Server/DTOs/Quiz/QuizQuestionDto.cs
@@ -1,9 +1,14 @@
-﻿namespace WatchersWorld.Server.DTOs.Quiz
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using WatchersWorld.Server.Models.Quiz;
+
+namespace WatchersWorld.Server.DTOs.Quiz
 {
     public class QuizQuestionDto
     {
 
-
+        public int Id { get; set; }
+        public int IdQuizQuestions { get; set; }
+        public int IdQuizOption { get; set; }
 
     }
 }
