307a5851fc6be04cd87d4e72ed5f78adecf1ece5
Diogo Carreira <202100265@estudantes.ips.pt>
2024-03-26 20:27:23 +0000

    gamification component

---
+++ WatchersWorld/watchersworld.client/src/app/gamification/gamification.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gamification',
+  templateUrl: './gamification.component.html',
+  styleUrl: './gamification.component.css'
+})
+export class GamificationComponent {
+
+}
