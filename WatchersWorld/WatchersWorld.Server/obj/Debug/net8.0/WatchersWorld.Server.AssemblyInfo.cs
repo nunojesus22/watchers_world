09e506a76a05e4affa5550d58971ca9c8beece0d
Joao202100233 <202100233@estudantes.ips.pt>
2024-04-08 16:04:20 +0100

    alterações cache 16:04

--- WatchersWorld/WatchersWorld.Server/obj/Debug/net8.0/WatchersWorld.Server.AssemblyInfo.cs
+++ WatchersWorld/WatchersWorld.Server/obj/Debug/net8.0/WatchersWorld.Server.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WatchersWorld.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31a21ba92ce766c3e01443527b4ffbd8beb3092e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37e7820ecb7bead57cb7d3452dd146d1d0e113bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("WatchersWorld.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WatchersWorld.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
