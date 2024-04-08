3a2a5824699beba6256d63e00ea72cc492944c25
Joao202100233 <202100233@estudantes.ips.pt>
2024-04-08 17:25:14 +0100

    dependencias signalR para azure

--- WatchersWorld/WatchersWorld.Server/obj/Debug/net8.0/WatchersWorld.Server.AssemblyInfo.cs
+++ WatchersWorld/WatchersWorld.Server/obj/Debug/net8.0/WatchersWorld.Server.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WatchersWorld.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65daf6225d974e9ab04e1386d0900189a3e4772c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e49c7994bf25528c66fcc47fda918af2047701d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("WatchersWorld.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WatchersWorld.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
