9f0e8c1cd3207e737106c00b6e4f8162fbe4b88c
Diogo Carreira <202100265@estudantes.ips.pt>
2024-04-09 19:21:14 +0100

    documentação

--- WatchersWorld/watchersworld.client/src/app/admin/service/admin.service.ts
+++ WatchersWorld/watchersworld.client/src/app/admin/service/admin.service.ts
@@ -6,7 +6,11 @@ import { User } from '../../authentication/models/user';
 import { environment } from '../../../environments/environment.development';
 import { Profile } from '../../profile/models/profile';
 
-
+/**
+ * Este serviço é responsável pela gestão de operações administrativas,
+ * incluindo a administração de utilizadores, a alteração de papéis de utilizadores,
+ * e a recolha de estatísticas gerais sobre o sistema.
+ */
 @Injectable({
   providedIn: 'root'
 })
@@ -14,6 +18,12 @@ export class AdminService {
 
   constructor(private http: HttpClient, private router: Router) { }
 
+  /**
+   * Recupera o token JWT do localStorage do navegador. Este token é utilizado para
+   * autenticação nas requisições à API.
+   * 
+   * @returns O token JWT ou 'No JWT' se o token não estiver presente.
+   */
   getJWT() {
     const key = localStorage.getItem(environment.userKey);
     if (key) {
@@ -24,6 +34,11 @@ export class AdminService {
     }
   }
 
+  /**
+   * Constrói os cabeçalhos HTTP para inclusão do token JWT nas requisições à API.
+   * 
+   * @returns Um objeto HttpHeaders configurado com o tipo de conteúdo e o token JWT.
+   */
   getHeaders() {
     const jwt = this.getJWT();
 
@@ -36,11 +51,24 @@ export class AdminService {
     return headers;
   }
 
+  /**
+  * Proíbe permanentemente um utilizador de aceder ao sistema.
+  * 
+  * @param username O nome de utilizador a ser banido permanentemente.
+  * @returns Um Observable que emite o resultado da operação de proibição.
+  */
   banUserPermanently(username: string): Observable<any> {
     const headers = this.getHeaders();
     return this.http.post<any>(`${environment.appUrl}/api/admin/ban-user-permanently/${encodeURIComponent(username)}`, {}, { headers });
   }
 
+  /**
+   * Proíbe temporariamente um utilizador de aceder ao sistema.
+   * 
+   * @param username O nome de utilizador a ser banido temporariamente.
+   * @param banDurationInDays A duração da proibição em dias.
+   * @returns Um Observable que emite o resultado da operação de proibição.
+   */
   BanUserTemporarily(username: string, banDurationInDays: number): Observable<any> {
     const headers = this.getHeaders();
     // Append the ban duration as a query parameter
@@ -48,56 +76,115 @@ export class AdminService {
     return this.http.post<any>(url, {}, { headers });
   }
 
+  /**
+   * Apaga um utilizador do sistema pelo seu nome de utilizador.
+   * 
+   * @param username O nome de utilizador do utilizador a ser apagado.
+   * @returns Um Observable que emite o resultado da operação de apagar.
+   */
   deleteUserByUsername(username: string): Observable<any> {
     const headers = this.getHeaders();
     return this.http.delete(`${environment.appUrl}/api/admin/users/${encodeURIComponent(username)}`,
       { headers, responseType: 'text' }); // Expecting a text response
   }
 
+  /**
+  * Obtém o papel de um utilizador no sistema.
+  * 
+  * @param username O nome de utilizador do utilizador cujo papel é solicitado.
+  * @returns Um Observable que emite os papéis do utilizador.
+  */
   getUserRole(username: string): Observable<string[]> {
     return this.http.get<string[]>(`${environment.appUrl}/api/admin/getUserRole/${username}`);
   }
 
+  /**
+   * Revoga a proibição de um utilizador, permitindo o seu acesso ao sistema novamente.
+   * 
+   * @param username O nome de utilizador do utilizador a ser desproibido.
+   * @returns Um Observable que emite o resultado da operação de desproibição.
+   */
   unbanUser(username: string): Observable<any> {
     const headers = this.getHeaders();
     return this.http.put<any>(`${environment.appUrl}/api/admin/unban-user/${encodeURIComponent(username)}`, {}, { headers });
   }
 
+  /**
+   * Altera o papel de um utilizador para Moderador.
+   * 
+   * @param username O nome de utilizador do utilizador cujo papel será alterado para Moderador.
+   * @returns Um Observable que emite o resultado da operação de alteração de papel.
+   */
   changeRoleToModerator(username: string): Observable<any> {
     const headers = this.getHeaders();
     return this.http.put(`${environment.appUrl}/api/admin/change-role-to-moderator/${encodeURIComponent(username)}`, {}, {
       headers: headers,
-      responseType: 'text'  // Expect a text response
+      responseType: 'text'
     });
   }
 
+  /**
+   * Altera o papel de um utilizador para Utilizador.
+   * 
+   * @param username O nome de utilizador do utilizador cujo papel será alterado para Utilizador.
+   * @returns Um Observable que emite o resultado da operação de alteração de papel.
+   */
   changeRoleToUser(username: string): Observable<any> {
     const headers = this.getHeaders();
     return this.http.put(`${environment.appUrl}/api/admin/change-role-to-user/${encodeURIComponent(username)}`, {}, {
       headers: headers,
-      responseType: 'text'  // Expect a text response
+      responseType: 'text'
     });
   }
-  //ESTATISTICAS
+
+  /**
+   * Obtém o número total de utilizadores registados no sistema.
+   * 
+   * @returns Um Observable que emite o número total de utilizadores registados.
+   */
   getTotalRegisteredUsers(): Observable<number> {
     const headers = this.getHeaders();
     return this.http.get<number>(`${environment.appUrl}/api/admin/total-registered-users`, { headers });
   }
 
+  /**
+   * Obtém o número total de utilizadores proibidos no sistema.
+   * 
+   * @returns Um Observable que emite o número total de utilizadores proibidos.
+   */
   getTotalBannedUsers(): Observable<number> {
-    return this.http.get<number>(`${environment.appUrl}/api/admin/total-banned-users`);
+    const headers = this.getHeaders();
+    return this.http.get<number>(`${environment.appUrl}/api/admin/total-banned-users`, { headers });
   }
 
+  /**
+   * Obtém o número total de perfis privados no sistema.
+   * 
+   * @returns Um Observable que emite o número total de perfis privados.
+   */
   getTotalPrivateProfiles(): Observable<number> {
-    return this.http.get<number>(`${environment.appUrl}/api/admin/total-private-profiles`);
+    const headers = this.getHeaders();
+    return this.http.get<number>(`${environment.appUrl}/api/admin/total-private-profiles`, { headers });
   }
 
+  /**
+  * Obtém o número total de perfis públicos no sistema.
+  * 
+  * @returns Um Observable que emite o número total de perfis públicos.
+  */
   getTotalPublicProfiles(): Observable<number> {
-    return this.http.get<number>(`${environment.appUrl}/api/admin/total-public-profiles`);
+    const headers = this.getHeaders();
+    return this.http.get<number>(`${environment.appUrl}/api/admin/total-public-profiles`, { headers });
   }
 
+  /**
+   * Obtém o número total de comentários feitos no sistema.
+   * 
+   * @returns Um Observable que emite o número total de comentários.
+   */
   getTotalComments(): Observable<number> {
-    return this.http.get<number>(`${environment.appUrl}/api/admin/total-comments`);
+    const headers = this.getHeaders();
+    return this.http.get<number>(`${environment.appUrl}/api/admin/total-comments`, { headers });
   }
 
 }
--- WatchersWorld/watchersworld.client/src/app/gamification/Service/gamification.service.ts
+++ WatchersWorld/watchersworld.client/src/app/gamification/Service/gamification.service.ts
@@ -6,17 +6,24 @@ import { User } from '../../authentication/models/user';
 import { Observable } from 'rxjs';
 import { MedalsDto } from '../models/MedalsDto';
 
+/**
+ * Este serviço é responsável pela gestão de operações relacionadas à gamificação,
+ * como a obtenção de medalhas desbloqueadas e bloqueadas pelos utilizadores, a atribuição de medalhas
+ * e a recolha de informações sobre todas as medalhas disponíveis.
+ */
 @Injectable({
   providedIn: 'root'
 })
 export class GamificationService {
 
-  
-
   constructor(private http: HttpClient) { }
 
- 
-
+  /**
+   * Recupera o token JWT do localStorage do navegador. Este token é utilizado para
+   * autenticação nas requisições à API.
+   * 
+   * @returns O token JWT ou 'No JWT' se o token não estiver presente.
+   */
   getJWT() {
     const key = localStorage.getItem(environment.userKey);
     if (key) {
@@ -27,6 +34,11 @@ export class GamificationService {
     }
   }
 
+  /**
+   * Constrói os cabeçalhos HTTP para inclusão do token JWT nas solicitações à API.
+   * 
+   * @returns Um objeto HttpHeaders configurado com o tipo de conteúdo e o token JWT.
+   */
   getHeaders() {
     const jwt = this.getJWT();
 
@@ -39,24 +51,44 @@ export class GamificationService {
     return headers;
   }
 
-
-
+  /**
+   * Obtém as medalhas desbloqueadas por um determinado utilizador.
+   * 
+   * @param userName O nome do utilizador cujas medalhas desbloqueadas são solicitadas.
+   * @returns Um Observable que emite uma lista de medalhas desbloqueadas (MedalsDto[]).
+   */
   getUnlockedMedals(userName: string): Observable<MedalsDto[]> {
     const headers = this.getHeaders();
     return this.http.get<MedalsDto[]>(`${environment.appUrl}/api/gamification/unlocked-medals/${userName}`, { headers });
   }
 
-
+  /**
+  * Atribui uma medalha a um utilizador específico.
+  * 
+  * @param userName O nome do utilizador ao qual a medalha será atribuída.
+  * @param medalName O nome da medalha a ser atribuída.
+  * @returns Um Observable que emite a lista atualizada de medalhas do utilizador após a atribuição.
+  */
   awardMedal(userName: string, medalName: string): Observable<MedalsDto[]> {
     const headers = this.getHeaders();
     return this.http.post<MedalsDto[]>(`${environment.appUrl}/api/gamification/AwardMedal`, { userName, medalName }, { headers });
   }
 
-
+  /**
+   * Obtém todas as medalhas disponíveis no sistema.
+   * 
+   * @returns Um Observable que emite uma lista de todas as medalhas disponíveis (MedalsDto[]).
+   */
   getAvailableMedals(): Observable<MedalsDto[]> {
     return this.http.get<MedalsDto[]>(`${environment.appUrl}/api/gamification/available-medals`);
   }
 
+  /**
+   * Obtém as medalhas que ainda estão bloqueadas para um determinado utilizador.
+   * 
+   * @param userName O nome do utilizador cujas medalhas bloqueadas são solicitadas.
+   * @returns Um Observable que emite uma lista de medalhas bloqueadas (MedalsDto[]).
+   */
   getLockedMedals(userName: string): Observable<MedalsDto[]> {
     const headers = this.getHeaders();
     return this.http.get<MedalsDto[]>(`${environment.appUrl}/api/gamification/locked-medals/${userName}`, { headers });
