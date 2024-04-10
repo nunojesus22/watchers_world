eaa5185a82cd4742d1784a1a861e3c9ad8cb2feb
Joao202100233 <202100233@estudantes.ips.pt>
2024-04-10 19:41:17 +0100

    warning fixes

--- WatchersWorld/watchersworld.client/src/app/nav-menu/nav-menu.component.ts
+++ WatchersWorld/watchersworld.client/src/app/nav-menu/nav-menu.component.ts
@@ -8,6 +8,11 @@ import { Subject, takeUntil } from 'rxjs';
 import { FollowerProfile } from '../profile/models/follower-profile';
 import { NotificationService } from '../notifications/services/notification.service';
 
+/**
+ * Este componente representa o menu de navegação da aplicação, oferecendo funcionalidades
+ * como a gestão da autenticação do utilizador, navegação baseada em papel de utilizador, 
+ * pesquisa de media e visualização de notificações e solicitações de seguimento pendentes.
+ */
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
@@ -20,16 +25,24 @@ export class NavMenuComponent {
   unsubscribed$: Subject<void> = new Subject<void>();
   pendingFollowRequests: FollowerProfile[] = [];
   hasUnreadNotifications: boolean = false;
+  showMenu = false;
 
+  constructor(private profileService: ProfileService, private notificationService: NotificationService, public authService: AuthenticationService, private _eref: ElementRef, public router: Router) {}
 
-  constructor(private service: MovieApiServiceComponent, private profileService: ProfileService, private notificationService: NotificationService, public authService: AuthenticationService, private _eref: ElementRef, public router: Router, private searchService: SearchServiceComponent) {}
-
+  /**
+   * Inicializa as propriedades e verifica o estado de autenticação do utilizador.
+   * Carrega as solicitações de seguimento pendentes e verifica a existência de notificações não lidas.
+   */
   ngOnInit(): void {
     this.loggedUserName = this.authService.getLoggedInUserName();
     this.getPendingFollowRequests();
     this.checkForUnreadNotifications();
   }
 
+  /**
+   * Carrega as solicitações de seguimento pendentes do utilizador autenticado.
+   * Atualiza a lista de solicitações pendentes no menu de navegação.
+   */
   getPendingFollowRequests() {
     if (this.loggedUserName) {
       this.profileService.getPendingFollowRequests(this.loggedUserName)
@@ -45,6 +58,10 @@ export class NavMenuComponent {
     }
   }
 
+  /**
+   * Verifica se existem notificações não lidas para o utilizador autenticado.
+   * Atualiza o indicador de notificações não lidas no menu de navegação.
+   */
   checkForUnreadNotifications(): void {
     if (this.loggedUserName) {
       this.notificationService.hasUnreadNotifications(this.loggedUserName)
@@ -59,7 +76,13 @@ export class NavMenuComponent {
     }
   }
 
-  showMenu = false;
+  
+  /**
+   * Gerencia a exibição do menu de navegação lateral baseando-se na interação do utilizador.
+   * Fecha o menu lateral quando um clique é registrado fora do menu.
+   * 
+   * @param event O evento de clique documentado.
+   */
   @HostListener('document:click', ['$event'])
   clickout(event: MouseEvent) {
     if (!this._eref.nativeElement.contains(event.target)) {
@@ -70,6 +93,12 @@ export class NavMenuComponent {
       }
     }
   }
+
+  /**
+   * Alterna a visibilidade do menu de navegação lateral.
+   * 
+   * @param event O evento de clique que desencadeou a alteração.
+   */
   toggleMenu(event: MouseEvent) {
     event.stopPropagation();
     this.showMenu = !this.showMenu;
@@ -83,15 +112,20 @@ export class NavMenuComponent {
     }
   }
 
+  /**
+   * Fecha o menu de navegação lateral.
+   */
   closeNav() {
     this.showMenu = false;
     let sidebar = document.getElementById("sidebar");
     if (sidebar) {
       sidebar.style.width = "0";
     }
-
   }
 
+  /**
+   * Submete uma consulta de pesquisa e navega para a página de resultados da pesquisa.
+   */
   onSubmit() {
     if (this.searchQuery) {
       // Isso garante que a pesquisa é comunicada através de parâmetros de consulta
@@ -99,10 +133,18 @@ export class NavMenuComponent {
     }
   }
 
+  /**
+   * Lida com o evento de tecla levantada no campo de pesquisa.
+   */
   onKeyup() {
     console.log('one key up',this.searchQuery);
   }
 
+  /**
+   * Navega para diferentes partes da aplicação com base no papel do utilizador autenticado.
+   * 
+   * @param username O nome de utilizador do utilizador autenticado.
+   */
   navigateBasedOnRole(username: string) {
     this.authService.getUserRole(username).subscribe((roles: string[]) => {
       if (roles.includes('Admin')) {
@@ -121,6 +163,9 @@ export class NavMenuComponent {
     });
   }
 
+  /**
+   * Realiza o logout do utilizador e redireciona para a página de login.
+   */
   logout() {
     this.authService.logout();
   }
